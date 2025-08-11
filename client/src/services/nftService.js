53f7dd52f322bbd9bea3959ad87f7e4305100abd
Aren <Aren.silver12@gmail.com>
2025-08-11 16:33:28 +0330

    Create nftService.js

---
+++ client/src/services/nftService.js
@@ -0,0 +1,11 @@
+export const getCreatedNFTs = async (userAddress) => {
+  const response = await fetch(`/api/nfts/created?address=${userAddress}`)
+  if (!response.ok) throw new Error('Failed to fetch created NFTs')
+  return await response.json()
+}
+
+export const getCollectedNFTs = async (userAddress) => {
+  const response = await fetch(`/api/nfts/collected?address=${userAddress}`)
+  if (!response.ok) throw new Error('Failed to fetch collected NFTs')
+  return await response.json()
+}
