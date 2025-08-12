87fd3a5c73dcc5213a9c3939c23f1c40a91c2ba3
Aren <Aren.silver12@gmail.com>
2025-08-12 17:53:27 +0330

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Mint from './pages/Mint';
-import Profile from './pages/Profile';
-import Upload from './pages/Upload';
-import './styles/theme.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+import Profile from './pages/Profile'
+import Mint from './pages/Mint'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mint" element={<Mint />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/mint/:id" element={<Mint />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
