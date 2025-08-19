458ff971699bf33f4d8bdfc24b5f796b61a2cdc0
Aren <Aren.silver12@gmail.com>
2025-08-20 02:33:01 +0330

    Update lib.rs

--- programs/arciumint-nft-gen/src/lib.rs
+++ programs/arciumint-nft-gen/src/lib.rs
@@ -1,11 +1,47 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, TokenAccount, Token, MintTo, mint_to};
+
+declare_id!("Hx6jdiv9A7LVoa83LYRNXb2SaTGTPdfKQ75hB3hyRuHW");
+
+#[program]
+pub mod arciumintnftgen {
+    use super::*;
+
+    pub fn mint_nft(ctx: Context<MintNFT>) -> Result<()> {
+        let user_record = &mut ctx.accounts.user_record;
+
+        require!(!user_record.has_minted, ErrorCode::AlreadyMinted);
+
+        let signer_seeds: &[&[&[u8]]] = &[&[b"mint_authority", &[ctx.bumps.mint_authority]]];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            MintTo {
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.token_account.to_account_info(),
+                authority: ctx.accounts.mint_authority.to_account_info(),
+            },
+            signer_seeds,
+        );
+
+        mint_to(cpi_ctx, 1)?;
+        user_record.has_minted = true;
+
+        Ok(())
+    }
+}
+
 #[derive(Accounts)]
 pub struct MintNFT<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
 
     #[account(
+        init_if_needed,
         seeds = [b"userrecord", authority.key().as_ref()],
-        bump
+        bump,
+        payer = authority,
+        space = 8 + 1
     )]
     pub user_record: Account<'info, UserRecord>,
 
@@ -15,7 +51,21 @@ pub struct MintNFT<'info> {
     #[account(mut)]
     pub token_account: Account<'info, TokenAccount>,
 
+    #[account(seeds = [b"mint_authority"], bump)]
+    pub mint_authority: UncheckedAccount<'info>,
+
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
+}
+
+#[account]
+pub struct UserRecord {
+    pub has_minted: bool,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("This wallet has already minted an NFT.")]
+    AlreadyMinted,
 }
