8f49e49c3ac228ff180ed7965a9e497d94d5adf3
Aren <Aren.silver12@gmail.com>
2025-08-23 16:03:47 +0330

    Update lib.rs

--- programs/arciumint-nft-gen/src/lib.rs
+++ programs/arciumint-nft-gen/src/lib.rs
@@ -1,8 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, TokenAccount, Token, MintTo, mint_to};
-use mpl_token_metadata::state::{DataV2};
-use mpl_token_metadata::instruction::{create_metadata_accounts_v3};
-use anchor_lang::solana_program::program::invoke_signed;
+use mpl_token_metadata::instruction::create_metadata_accounts_v3;
 
 declare_id!("22aiFCK8g424HHtkhcZfJTrCx34eQMcRHNgsWGyXB8Vn");
 
@@ -35,16 +33,6 @@ pub mod arciumintnftgen {
         );
         mint_to(cpi_ctx, 1)?;
 
-        let data = DataV2 {
-            name,
-            symbol,
-            uri,
-            seller_fee_basis_points: 0,
-            creators: None,
-            collection: None,
-            uses: None,
-        };
-
         let ix = create_metadata_accounts_v3(
             ctx.accounts.token_metadata_program.key(),
             ctx.accounts.metadata.key(),
@@ -52,7 +40,11 @@ pub mod arciumintnftgen {
             ctx.accounts.mint_authority.key(),
             ctx.accounts.signer.key(),
             ctx.accounts.mint_authority.key(),
-            data,
+            name,
+            symbol,
+            uri,
+            None,
+            1,
             true,
             false,
             None,
@@ -60,7 +52,7 @@ pub mod arciumintnftgen {
             None,
         );
 
-        invoke_signed(
+        anchor_lang::solana_program::program::invoke_signed(
             &ix,
             &[
                 ctx.accounts.metadata.to_account_info(),
