131040ea840274fc29593823cd1ec98df4dab24a
Aren <Aren.silver12@gmail.com>
2025-08-24 15:14:58 +0330

    Update metadata.rs

--- programs/arciumint-nft-gen/src/metadata.rs
+++ programs/arciumint-nft-gen/src/metadata.rs
@@ -1,64 +1,44 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::{program::invoke_signed, pubkey::Pubkey};
-use mpl_token_metadata::instructions::CreateMetadataAccountsV3;
-use mpl_token_metadata::types::{Creator, DataV2};
+use solana_program::program::invoke_signed;
+use mpl_token_metadata::instructions::CreateMetadataAccountV3CpiBuilder;
 
-#[allow(clippy::too_many_arguments)]
 pub fn create_metadata<'info>(
-    metadata_program: AccountInfo<'info>,
-    metadata_account: Pubkey,
-    mint: Pubkey,
-    mint_authority: Pubkey,
-    payer: Pubkey,
-    update_authority: Pubkey,
+    token_metadata_program: AccountInfo<'info>,
+    metadata_account: AccountInfo<'info>,
+    mint: AccountInfo<'info>,
+    mint_authority: AccountInfo<'info>,
+    payer: AccountInfo<'info>,
     name: String,
     symbol: String,
     uri: String,
-    system_program: AccountInfo<'info>,
-    metadata_ai: AccountInfo<'info>,
-    mint_ai: AccountInfo<'info>,
-    mint_authority_ai: AccountInfo<'info>,
-    payer_ai: AccountInfo<'info>,
-    update_authority_ai: AccountInfo<'info>,
     signer_seeds: &[&[&[u8]]],
 ) -> Result<()> {
-    let creators = vec![Creator {
-        address: payer,
-        verified: true,
-        share: 100,
-    }];
+    let mut builder = CreateMetadataAccountV3CpiBuilder::new(&token_metadata_program);
 
-    let data_v2 = DataV2 {
-        name,
-        symbol,
-        uri,
-        seller_fee_basis_points: 500,
-        creators: Some(creators),
-        collection: None,
-        uses: None,
-    };
+    builder
+        .metadata(&metadata_account)
+        .mint(&mint)
+        .mint_authority(&mint_authority)
+        .payer(&payer)
+        .update_authority(&mint_authority, true)
+        .name(name)
+        .symbol(symbol)
+        .uri(uri)
+        .is_mutable(true);
 
-    let ix = CreateMetadataAccountsV3 {
-        metadata: metadata_account,
-        mint,
-        mint_authority,
-        payer,
-        update_authority,
-        system_program: system_program.key(),
-        rent: None,
-    }
-    .instruction(data_v2);
+    let ix = builder.instruction();
 
-    let accounts = &[
-        metadata_ai,
-        mint_ai,
-        mint_authority_ai,
-        payer_ai,
-        update_authority_ai,
-        system_program,
-    ];
-
-    invoke_signed(&ix, accounts, signer_seeds)?;
+    invoke_signed(
+        &ix,
+        &[
+            metadata_account,
+            mint,
+            mint_authority,
+            payer,
+            token_metadata_program,
+        ],
+        signer_seeds,
+    )?;
 
     Ok(())
 }
