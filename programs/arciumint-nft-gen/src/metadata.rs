0bf6542d8d9a759cd76958ee1a3653a1a5bf8d26
Aren <Aren.silver12@gmail.com>
2025-08-24 04:57:22 +0330

    Update metadata.rs

--- programs/arciumint-nft-gen/src/metadata.rs
+++ programs/arciumint-nft-gen/src/metadata.rs
@@ -1,19 +1,17 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::{
-    instruction::Instruction,
     program::invoke_signed,
     pubkey::Pubkey,
 };
-use mpl_token_metadata::types::Creator;
-use mpl_token_metadata::accounts::CreateMetadataAccountsV3;
-use mpl_token_metadata::instructions::create_metadata_accounts_v3;
+use mpl_token_metadata::instruction::create_metadata_accounts_v3;
+use mpl_token_metadata::state::Creator;
 
 pub fn create_metadata<'info>(
     metadata_program: AccountInfo<'info>,
     metadata_account: Pubkey,
-    mint: AccountInfo<'info>,
-    mint_authority: AccountInfo<'info>,
-    payer: AccountInfo<'info>,
+    mint: Pubkey,
+    mint_authority: Pubkey,
+    payer: Pubkey,
     update_authority: Pubkey,
     name: String,
     symbol: String,
@@ -25,33 +23,28 @@ pub fn create_metadata<'info>(
     signer_seeds: &[&[&[u8]]],
 ) -> Result<()> {
     let creators = vec![Creator {
-        address: payer.key(), // Pubkey از anchor_lang::solana_program
+        address: payer,
         verified: true,
         share: 100,
     }];
 
-    let accounts = CreateMetadataAccountsV3 {
-        metadata: metadata_account,
-        mint: *mint.key,
-        mint_authority: *mint_authority.key,
-        payer: *payer.key,
+    let ix = create_metadata_accounts_v3(
+        metadata_program.key(),
+        metadata_account,
+        mint,
+        mint_authority,
+        payer,
         update_authority,
-        system_program: *system_program.key,
-        rent: *rent.key,
-    };
-
-    let ix: Instruction = create_metadata_accounts_v3(
-        accounts,
         name,
         symbol,
         uri,
         Some(creators),
-        500,  // Royalty: 5%
-        true, // Primary sale happened
-        true, // Is mutable
-        None, // Collection
-        None, // Uses
-        None, // RuleSet
+        500,
+        true,
+        true,
+        None,
+        None,
+        None,
     );
 
     invoke_signed(
@@ -59,9 +52,6 @@ pub fn create_metadata<'info>(
         &[
             metadata_program,
             metadata,
-            mint,
-            mint_authority,
-            payer,
             system_program,
             rent,
         ],
