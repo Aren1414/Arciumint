aced3fa81aa5e24a188290117954dbf8d8095da2
Aren <Aren.silver12@gmail.com>
2025-08-31 21:52:48 +0330

    Update lib.rs

--- programs/arciumint-nft-gen/src/lib.rs
+++ programs/arciumint-nft-gen/src/lib.rs
@@ -2,6 +2,9 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::{self, mint_to, Mint, MintTo, Token, TokenAccount};
 
 pub mod metadata;
+pub mod accounts;
+
+use accounts::*; 
 
 declare_id!("22aiFCK8g424HHtkhcZfJTrCx34eQMcRHNgsWGyXB8Vn");
 
@@ -50,50 +53,6 @@ fn mint_token_to_user<'info>(
 
     mint_to(cpi_ctx, 1)?;
     Ok(())
-}
-
-#[derive(Accounts)]
-pub struct MintNFT<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>, 
-
-    #[account(
-        init_if_needed,
-        payer = payer,
-        space = 8 + UserRecord::SIZE,
-        seeds = [b"user_record", payer.key().as_ref()],
-        bump
-    )]
-    pub user_record: Account<'info, UserRecord>,
-
-    #[account(mut)]
-    pub mint: Account<'info, Mint>,
-
-    #[account(mut)]
-    pub token_account: Account<'info, TokenAccount>,
-
-    #[account(seeds = [b"mint_authority"], bump)]
-    /// CHECK: PDA signer (no data read)
-    pub mint_authority: UncheckedAccount<'info>,
-
-    #[account(mut)]
-    /// CHECK: to be created/initialized by Metaplex CPI
-    pub metadata: UncheckedAccount<'info>,
-
-    /// CHECK: treated as program id in CPI
-    pub token_metadata_program: UncheckedAccount<'info>,
-
-    pub token_program: Program<'info, Token>,
-    pub system_program: Program<'info, System>,
-    pub rent: Sysvar<'info, Rent>,
-}
-
-#[account]
-pub struct UserRecord {
-    pub has_minted: bool,
-}
-impl UserRecord {
-    pub const SIZE: usize = 1;
 }
 
 #[error_code]
