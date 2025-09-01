e21c470bc28d974b173408d207785b3aee08d2cf
Aren <Aren.silver12@gmail.com>
2025-09-02 02:48:04 +0330

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()]
+    }
+  },
+  resolve: {
+    alias: {
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+    }
+  }
 });
