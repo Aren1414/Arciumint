3d8aa97f2348cc710a766044bf072fdae2f66eb9
Aren1414 <aren.silver12@gmail.com>
2025-09-01 16:53:34 +0330

    chore: remove unused server folder

--- server/routes/nftRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllNFTs, createNFT } = require('../controllers/nftController');
-
-router.get('/', getAllNFTs);
-router.post('/', createNFT);
-
-module.exports = router;
