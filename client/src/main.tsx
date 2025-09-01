335f112e3a1917781c5cba24d7217c14b6aaed88
Aren <Aren.silver12@gmail.com>
2025-09-01 17:09:26 +0330

    Update and rename main.jsx to main.tsx

---
+++ client/src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles/theme.css';
+import Home from './pages/Home';
+import Upload from './pages/Upload';
+import Profile from './pages/Profile';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<App />);
+}
