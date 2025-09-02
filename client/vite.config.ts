216f3b54557d6d84419eb5e46bac94ab6b430ea7
Aren <Aren.silver12@gmail.com>
2025-09-02 22:18:35 +0330

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      stream: "stream-browserify",
+      util: "util",
+      buffer: "buffer",
+      crypto: "crypto-browserify",
+      events: "events",
+    },
+  },
 });
