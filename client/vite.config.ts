c4e2be46a19e409f48b319cdea80d124e9e0c37e
Aren <Aren.silver12@gmail.com>
2025-09-02 20:53:45 +0330

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import inject from "@rollup/plugin-inject";
-
 
 export default defineConfig({
-  base: "/", 
-  define: {
-    global: "globalThis", 
-  },
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -18,27 +11,6 @@ export default defineConfig({
       buffer: "buffer",
       crypto: "crypto-browserify",
       events: "events",
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        inject({
-          Buffer: ["buffer", "Buffer"],
-          process: "process",
-        }),
-      ],
     },
   },
 });
