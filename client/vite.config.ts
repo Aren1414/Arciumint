a1f75817a3a7191de75c18f8c7c492c9cd2b75b6
Aren <Aren.silver12@gmail.com>
2025-09-03 02:19:50 +0330

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+        "process.env": {},
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
   resolve: {
     alias: {
       stream: "stream-browserify",
@@ -11,6 +28,12 @@ export default defineConfig({
       buffer: "buffer",
       crypto: "crypto-browserify",
       events: "events",
+      assert: "assert",
+      http: "stream-http",
+      https: "https-browserify",
+      os: "os-browserify/browser",
+      url: "url",
+      zlib: "browserify-zlib",
     },
   },
 });
