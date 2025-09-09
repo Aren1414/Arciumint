1157ca8866ad4d7204832110ab494cd05bd37aae
Aren <Aren.silver12@gmail.com>
2025-09-10 02:51:47 +0330

    Update lib.rs

--- programs/arciumint-nft-gen/src/lib.rs
+++ programs/arciumint-nft-gen/src/lib.rs
@@ -1,17 +1,11 @@
 use anchor_lang::prelude::*;
-
-#[cfg(not(feature = "exclude-accounts"))]
 use anchor_spl::token::{mint_to, Mint, MintTo, Token, TokenAccount};
-#[cfg(not(feature = "exclude-accounts"))]
 use anchor_spl::associated_token::AssociatedToken;
-#[cfg(not(feature = "exclude-accounts"))]
 use anchor_spl::metadata::{create_metadata_accounts_v3, CreateMetadataAccountsV3};
-#[cfg(not(feature = "exclude-accounts"))]
 use mpl_token_metadata::types::{Creator, DataV2, CollectionDetails};
 
 declare_id!("22aiFCK8g424HHtkhcZfJTrCx34eQMcRHNgsWGyXB8Vn");
 
-#[cfg(not(feature = "exclude-accounts"))]
 #[derive(Accounts)]
 pub struct MintNFT<'info> {
     #[account(mut)]
@@ -52,7 +46,6 @@ pub struct MintNFT<'info> {
     pub rent: Sysvar<'info, Rent>,
 }
 
-#[cfg(not(feature = "exclude-accounts"))]
 #[derive(Accounts)]
 pub struct MintNFTWithMPC<'info> {
     #[account(mut)]
@@ -93,12 +86,10 @@ pub struct MintNFTWithMPC<'info> {
     pub rent: Sysvar<'info, Rent>,
 }
 
-#[cfg(not(feature = "exclude-accounts"))]
 #[account]
 pub struct UserRecord {
     pub has_minted: bool,
 }
-#[cfg(not(feature = "exclude-accounts"))]
 impl UserRecord {
     pub const SIZE: usize = 1;
 }
@@ -264,7 +255,6 @@ fn create_metadata_for_token_mpc<'info>(
     Ok(())
 }
 
-#[cfg(not(feature = "exclude-accounts"))]
 #[error_code]
 pub enum ErrorCode {
     #[msg("This wallet has already minted an NFT.")]
