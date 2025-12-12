89a776ee5031e5b05e8ee221d02ae317ebe83666
Aren <Aren.silver12@gmail.com>
2025-12-12 19:02:43 +0330

    Update WalletConnectionProvider.tsx

--- web/components/WalletConnectionProvider.tsx
+++ web/components/WalletConnectionProvider.tsx
@@ -4,22 +4,24 @@ import { useMemo } from "react";
 import type { ReactElement } from "react";
 import {
   ConnectionProvider,
-  WalletProvider
+  WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function WalletConnectionProvider({ children }: Props): ReactElement {
-  const endpoint: string = "https://api.devnet.solana.com";
+  const endpoint = "https://api.devnet.solana.com";
+  const network = WalletAdapterNetwork.Devnet;
 
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const wallets = useMemo(() => [new PhantomWalletAdapter({ network })], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect={false}>
+      <WalletProvider wallets={wallets} autoConnect={true}>
         {children}
       </WalletProvider>
     </ConnectionProvider>
