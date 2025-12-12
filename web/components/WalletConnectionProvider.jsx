3c28f5448c150da5faf21c28274988a8d616fed9
Aren <Aren.silver12@gmail.com>
2025-12-12 16:29:27 +0330

    Update WalletConnectionProvider.jsx

--- web/components/WalletConnectionProvider.jsx
+++ web/components/WalletConnectionProvider.jsx
@@ -14,15 +14,10 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 export default function WalletConnectionProvider({ children }) {
   const endpoint = "https://api.devnet.solana.com";
 
-  // Mobile deep link MUST be provided
   const wallets = useMemo(() => {
     return [
       new PhantomWalletAdapter({
-        // APP DEEP LINK FOR MOBILE
-        appUrl: "https://arciumint.vercel.app",
-        // Manual override to prevent redirect-to-download
-        // Ensures mobile opens the app instead of website
-        supportedTransactionVersions: ["legacy", 0],
+        appUrl: "https://arciumint.vercel.app", 
       }),
     ];
   }, []);
