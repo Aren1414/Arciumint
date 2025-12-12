14ea3e186738bf3089dbd8dc907579be7aa24eb9
Aren <Aren.silver12@gmail.com>
2025-12-12 16:10:37 +0330

    Update WalletConnectionProvider.jsx

--- web/components/WalletConnectionProvider.jsx
+++ web/components/WalletConnectionProvider.jsx
@@ -9,7 +9,7 @@ import {
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
-require("@solana/wallet-adapter-react-ui/styles.css");
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function WalletConnectionProvider({ children }) {
   const endpoint = "https://api.devnet.solana.com";
