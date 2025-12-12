3c746a1f02e0fcd5e2688f67d980320f275fb3d6
Aren <Aren.silver12@gmail.com>
2025-12-13 00:31:26 +0330

    Update ClientWalletProvider.tsx

--- web/components/ClientWalletProvider.tsx
+++ web/components/ClientWalletProvider.tsx
@@ -1,36 +1,22 @@
 "use client";
 
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-
-import {
-  PhantomWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { useMemo } from "react";
+import type { ReactElement } from "react";
 
-require("@solana/wallet-adapter-react-ui/styles.css");
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
-export default function ClientWalletProvider({ children }: any) {
+export default function ClientWalletProvider({ children }: { children: React.ReactNode }): ReactElement {
   const endpoint = "https://api.devnet.solana.com";
+  const network = WalletAdapterNetwork.Devnet;
 
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter({
-        
-        preferMobile: true,
-      }),
-    ],
-    []
-  );
+  const wallets = useMemo(() => [new PhantomWalletAdapter({ network })], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
+      <WalletProvider wallets={wallets} autoConnect={true}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
