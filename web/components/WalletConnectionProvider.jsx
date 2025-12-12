df52bf8d61b092074ee03c0fbf6b70a600e2450c
Aren <Aren.silver12@gmail.com>
2025-12-12 16:37:31 +0330

    Update WalletConnectionProvider.jsx

--- web/components/WalletConnectionProvider.jsx
+++ web/components/WalletConnectionProvider.jsx
@@ -1,11 +1,7 @@
 "use client";
 
 import { useMemo } from "react";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
@@ -14,18 +10,16 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 export default function WalletConnectionProvider({ children }) {
   const endpoint = "https://api.devnet.solana.com";
 
-  const wallets = useMemo(() => {
-    return [
-      new PhantomWalletAdapter({
-        appUrl: "https://arciumint.vercel.app", 
-      }),
-    ];
-  }, []);
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter()
+  ], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect={false}>
-        <WalletModalProvider>{children}</WalletModalProvider>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
