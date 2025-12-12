111651236ec1d4c4a29348e95262aee34dcd130d
Aren <Aren.silver12@gmail.com>
2025-12-12 19:01:28 +0330

    Update ClientWalletProvider.tsx

--- web/components/ClientWalletProvider.tsx
+++ web/components/ClientWalletProvider.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import type { ReactElement } from "react";
 import WalletConnectionProvider from "./WalletConnectionProvider";
 
-export default function ClientWalletProvider({ children }: { children: React.ReactNode }) {
+export default function ClientWalletProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}): ReactElement {
   return <WalletConnectionProvider>{children}</WalletConnectionProvider>;
 }
