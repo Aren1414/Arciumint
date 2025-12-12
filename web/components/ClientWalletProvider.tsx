fc6d64e9b71030fe1019d0469127ca423b8dad5d
Aren <Aren.silver12@gmail.com>
2025-12-13 01:15:04 +0330

    Update ClientWalletProvider.tsx

--- web/components/ClientWalletProvider.tsx
+++ web/components/ClientWalletProvider.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { useMemo } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
-require("@solana/wallet-adapter-react-ui/styles.css");
-
-export default function ClientWalletProvider({ children }: any) {
+export default function ClientWalletProvider({ children }: { children: React.ReactNode }) {
   const endpoint = "https://api.devnet.solana.com";
 
   const wallets = useMemo(
