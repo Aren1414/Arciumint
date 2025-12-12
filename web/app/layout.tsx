fb2b3dd1ccf48d73c9c53dd12777c71e9221413e
Aren <Aren.silver12@gmail.com>
2025-12-13 00:52:50 +0330

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,23 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-/**
- * IMPORT GLOBAL CSS FOR WALLET-ADAPTER UI HERE (allowed only in layout/_app)
- */
-import "@solana/wallet-adapter-react-ui/styles.css";
-
 import ClientWalletProvider from "@/components/ClientWalletProvider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Arciumint",
@@ -27,8 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ClientWalletProvider>{children}</ClientWalletProvider>
+      <body>
+        <ClientWalletProvider>
+          {children}
+        </ClientWalletProvider>
       </body>
     </html>
   );
