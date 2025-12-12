61c6ace59f50a71662bec70ef423d98d11d8f927
Aren <Aren.silver12@gmail.com>
2025-12-12 15:43:50 +0330

    Update WalletConnectionProvider.jsx

--- web/components/WalletConnectionProvider.jsx
+++ web/components/WalletConnectionProvider.jsx
@@ -1,40 +1,36 @@
 "use client";
 
-import { useMemo, createContext, useContext } from "react";
+import { createContext, useContext, useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
-  useWallet,
 } from "@solana/wallet-adapter-react";
 
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContext = createContext(null);
 
+export function useWalletContext() {
+  return useContext(WalletContext);
+}
+
 export default function WalletConnectionProvider({ children }) {
   const endpoint = "https://api.devnet.solana.com";
 
-  const wallets = useMemo(
-    () => [new PhantomWalletAdapter()],
-    []
-  );
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
-          <WalletContext.Provider value={useWallet()}>
+          <WalletContext.Provider value={null}>
             {children}
           </WalletContext.Provider>
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
-}
-
-export function useWalletContext() {
-  return useContext(WalletContext);
 }
