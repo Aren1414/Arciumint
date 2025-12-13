a2d56fee76b5b3ef971d45bb134331109070d2f7
Aren <Aren.silver12@gmail.com>
2025-12-13 19:26:27 +0330

    Update SolanaProvider.tsx

--- web/components/SolanaProvider.tsx
+++ web/components/SolanaProvider.tsx
@@ -11,12 +11,14 @@ export default function SolanaProvider({
   children: React.ReactNode;
 }) {
   const endpoint = useMemo(() => "https://api.devnet.solana.com", []);
-
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect={false}>
+      <WalletProvider
+        wallets={wallets}
+        autoConnect={false}
+      >
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
