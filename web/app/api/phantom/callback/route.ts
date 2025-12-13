e905b760330c35e6938215d2b47a8e0eb884f35f
Aren <Aren.silver12@gmail.com>
2025-12-13 14:57:59 +0330

    Update route.ts

--- web/app/api/phantom/callback/route.ts
+++ web/app/api/phantom/callback/route.ts
@@ -1,6 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  
-  return NextResponse.redirect(new URL("/", request.url));
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+  const publicKey = url.searchParams.get("public_key");
+  const session = url.searchParams.get("session");
+
+  const redirectUrl = `/phantom-callback?public_key=${publicKey}&session=${session}`;
+
+  return NextResponse.redirect(redirectUrl);
 }
