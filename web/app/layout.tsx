cd7e79c51ebcd3449ba2d10368a96470808489b9
Aren <Aren.silver12@gmail.com>
2025-12-14 15:31:34 +0330

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
-import { PhantomProvider, darkTheme } from "@phantom/react-sdk";
-import { AddressType } from "@phantom/browser-sdk";
+import PhantomWrapper from "@/components/PhantomWrapper";
 import WalletComponent from "@/components/WalletComponent";
 
 export const metadata = {
@@ -12,22 +11,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <PhantomProvider
-          config={{
-            providers: ["injected"], 
-            appId: "e26969a5-9c5d-4e09-bf78-aa4a525a5d7f",
-            addressTypes: [AddressType.solana], 
-            authOptions: {
-              redirectUrl: "https://www.arciumint.xyz/phantom-callback",
-            },
-          }}
-          theme={darkTheme}
-          appIcon="https://phantom-portal20240925173430423400000001.s3.ca-central-1.amazonaws.com/icons/eefe3956-73d8-4000-ad3d-c0829043f32e.png"
-          appName="Arciumint"
-        >
+        <PhantomWrapper>
           {children}
           <WalletComponent />
-        </PhantomProvider>
+        </PhantomWrapper>
       </body>
     </html>
   );
