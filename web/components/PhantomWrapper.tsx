9458a9f45741600d15a6344a2f70c50148a8a4e1
Aren <Aren.silver12@gmail.com>
2025-12-14 17:24:18 +0330

    Update PhantomWrapper.tsx

--- web/components/PhantomWrapper.tsx
+++ web/components/PhantomWrapper.tsx
@@ -8,16 +8,13 @@ export default function PhantomWrapper({ children }: { children: ReactNode }) {
   return (
     <PhantomProvider
       config={{
-        providers: ["injected"],
         appId: "e26969a5-9c5d-4e09-bf78-aa4a525a5d7f",
+        providers: ["injected", "deeplink"],
         addressTypes: [AddressType.solana],
-        authOptions: {
-          redirectUrl: "https://www.arciumint.xyz/phantom-callback",
-        },
       }}
       theme={darkTheme}
-      appIcon="https://phantom-portal20240925173430423400000001.s3.ca-central-1.amazonaws.com/icons/eefe3956-73d8-4000-ad3d-c0829043f32e.png"
       appName="Arciumint"
+      appIcon="https://phantom-portal20240925173430423400000001.s3.ca-central-1.amazonaws.com/icons/eefe3956-73d8-4000-ad3d-c0829043f32e.png"
     >
       {children}
     </PhantomProvider>
