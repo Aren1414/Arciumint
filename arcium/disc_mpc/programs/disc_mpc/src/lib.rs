5e69fd21a5acbb7fa96cc4b909cc1a24b0052466
Aren <Aren.silver12@gmail.com>
2025-12-19 18:14:20 +0330

    Rename functions and constants for clarity

--- arcium/disc_mpc/programs/disc_mpc/src/lib.rs
+++ arcium/disc_mpc/programs/disc_mpc/src/lib.rs
@@ -1,7 +1,8 @@
 use anchor_lang::prelude::*;
 use arcium_anchor::prelude::*;
 
-const COMP_DEF_OFFSET_ADD_TOGETHER: u32 = comp_def_offset("add_together");
+// rename: add_together -> compute_disc
+const COMP_DEF_OFFSET_COMPUTE_DISC: u32 = comp_def_offset("compute_disc");
 
 declare_id!("A4EDNsvT5oGXVXFNvvetgJDzZmYySaWY773C784VXUoM");
 
@@ -9,13 +10,15 @@ declare_id!("A4EDNsvT5oGXVXFNvvetgJDzZmYySaWY773C784VXUoM");
 pub mod disc_mpc {
     use super::*;
 
-    pub fn init_add_together_comp_def(ctx: Context<InitAddTogetherCompDef>) -> Result<()> {
+    // rename
+    pub fn init_compute_disc_comp_def(ctx: Context<InitComputeDiscCompDef>) -> Result<()> {
         init_comp_def(ctx.accounts, None, None)?;
         Ok(())
     }
 
-    pub fn add_together(
-        ctx: Context<AddTogether>,
+    // rename
+    pub fn compute_disc(
+        ctx: Context<ComputeDisc>,
         computation_offset: u64,
         ciphertext_0: [u8; 32],
         ciphertext_1: [u8; 32],
@@ -23,6 +26,7 @@ pub mod disc_mpc {
         nonce: u128,
     ) -> Result<()> {
         ctx.accounts.sign_pda_account.bump = ctx.bumps.sign_pda_account;
+
         let args = ArgBuilder::new()
             .x25519_pubkey(pubkey)
             .plaintext_u128(nonce)
@@ -35,10 +39,10 @@ pub mod disc_mpc {
             computation_offset,
             args,
             None,
-            vec![AddTogetherCallback::callback_ix(
+            vec![ComputeDiscCallback::callback_ix(
                 computation_offset,
                 &ctx.accounts.mxe_account,
-                &[]
+                &[],
             )?],
             1,
             0,
@@ -46,13 +50,17 @@ pub mod disc_mpc {
         Ok(())
     }
 
-    #[arcium_callback(encrypted_ix = "add_together")]
-    pub fn add_together_callback(
-        ctx: Context<AddTogetherCallback>,
-        output: SignedComputationOutputs<AddTogetherOutput>,
+    // rename + encrypted_ix
+    #[arcium_callback(encrypted_ix = "compute_disc")]
+    pub fn compute_disc_callback(
+        ctx: Context<ComputeDiscCallback>,
+        output: SignedComputationOutputs<ComputeDiscOutput>,
     ) -> Result<()> {
-        let o = match output.verify_output(&ctx.accounts.cluster_account, &ctx.accounts.computation_account) {
-            Ok(AddTogetherOutput { field_0 }) => field_0,
+        let o = match output.verify_output(
+            &ctx.accounts.cluster_account,
+            &ctx.accounts.computation_account,
+        ) {
+            Ok(ComputeDiscOutput { field_0 }) => field_0,
             Err(_) => return Err(ErrorCode::AbortedComputation.into()),
         };
 
@@ -64,12 +72,14 @@ pub mod disc_mpc {
     }
 }
 
-#[queue_computation_accounts("add_together", payer)]
+// rename
+#[queue_computation_accounts("compute_disc", payer)]
 #[derive(Accounts)]
 #[instruction(computation_offset: u64)]
-pub struct AddTogether<'info> {
+pub struct ComputeDisc<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
+
     #[account(
         init_if_needed,
         space = 9,
@@ -79,88 +89,89 @@ pub struct AddTogether<'info> {
         address = derive_sign_pda!(),
     )]
     pub sign_pda_account: Account<'info, SignerAccount>,
-    #[account(
-        address = derive_mxe_pda!()
-    )]
+
+    #[account(address = derive_mxe_pda!())]
     pub mxe_account: Account<'info, MXEAccount>,
+
     #[account(
         mut,
         address = derive_mempool_pda!(mxe_account, ErrorCode::ClusterNotSet)
     )]
-    /// CHECK: mempool_account, checked by the arcium program.
+    /// CHECK
     pub mempool_account: UncheckedAccount<'info>,
+
     #[account(
         mut,
         address = derive_execpool_pda!(mxe_account, ErrorCode::ClusterNotSet)
     )]
-    /// CHECK: executing_pool, checked by the arcium program.
+    /// CHECK
     pub executing_pool: UncheckedAccount<'info>,
+
     #[account(
         mut,
         address = derive_comp_pda!(computation_offset, mxe_account, ErrorCode::ClusterNotSet)
     )]
-    /// CHECK: computation_account, checked by the arcium program.
+    /// CHECK
     pub computation_account: UncheckedAccount<'info>,
-    #[account(
-        address = derive_comp_def_pda!(COMP_DEF_OFFSET_ADD_TOGETHER)
-    )]
+
+    #[account(address = derive_comp_def_pda!(COMP_DEF_OFFSET_COMPUTE_DISC))]
     pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+
     #[account(
         mut,
         address = derive_cluster_pda!(mxe_account, ErrorCode::ClusterNotSet)
     )]
     pub cluster_account: Account<'info, Cluster>,
-    #[account(
-        mut,
-        address = ARCIUM_FEE_POOL_ACCOUNT_ADDRESS,
-    )]
+
+    #[account(mut, address = ARCIUM_FEE_POOL_ACCOUNT_ADDRESS)]
     pub pool_account: Account<'info, FeePool>,
-    #[account(
-        mut,
-        address = ARCIUM_CLOCK_ACCOUNT_ADDRESS
-    )]
+
+    #[account(mut, address = ARCIUM_CLOCK_ACCOUNT_ADDRESS)]
     pub clock_account: Account<'info, ClockAccount>,
+
     pub system_program: Program<'info, System>,
     pub arcium_program: Program<'info, Arcium>,
 }
 
-#[callback_accounts("add_together")]
+// rename
+#[callback_accounts("compute_disc")]
 #[derive(Accounts)]
-pub struct AddTogetherCallback<'info> {
+pub struct ComputeDiscCallback<'info> {
     pub arcium_program: Program<'info, Arcium>,
-    #[account(
-        address = derive_comp_def_pda!(COMP_DEF_OFFSET_ADD_TOGETHER)
-    )]
+
+    #[account(address = derive_comp_def_pda!(COMP_DEF_OFFSET_COMPUTE_DISC))]
     pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
-    #[account(
-        address = derive_mxe_pda!()
-    )]
+
+    #[account(address = derive_mxe_pda!())]
     pub mxe_account: Account<'info, MXEAccount>,
-    /// CHECK: computation_account, checked by arcium program via constraints in the callback context.
+
+    /// CHECK
     pub computation_account: UncheckedAccount<'info>,
+
     #[account(
         address = derive_cluster_pda!(mxe_account, ErrorCode::ClusterNotSet)
     )]
     pub cluster_account: Account<'info, Cluster>,
+
     #[account(address = ::anchor_lang::solana_program::sysvar::instructions::ID)]
-    /// CHECK: instructions_sysvar, checked by the account constraint
+    /// CHECK
     pub instructions_sysvar: AccountInfo<'info>,
 }
 
-#[init_computation_definition_accounts("add_together", payer)]
+// rename
+#[init_computation_definition_accounts("compute_disc", payer)]
 #[derive(Accounts)]
-pub struct InitAddTogetherCompDef<'info> {
+pub struct InitComputeDiscCompDef<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
-    #[account(
-        mut,
-        address = derive_mxe_pda!()
-    )]
+
+    #[account(mut, address = derive_mxe_pda!())]
     pub mxe_account: Box<Account<'info, MXEAccount>>,
+
     #[account(mut)]
-    /// CHECK: comp_def_account, checked by arcium program.
-    /// Can't check it here as it's not initialized yet.
+    /// CHECK
     pub comp_def_account: UncheckedAccount<'info>,
+
     pub arcium_program: Program<'info, Arcium>,
     pub system_program: Program<'info, System>,
 }
