00f1f911fbd4ca52df445888c4afd5401bcb58d3
Aren <Aren.silver12@gmail.com>
2025-12-22 23:56:36 +0330

    Update lib.rs

--- arcium/disc_mpc/programs/disc_mpc/src/lib.rs
+++ arcium/disc_mpc/programs/disc_mpc/src/lib.rs
@@ -17,9 +17,8 @@ pub mod disc_mpc {
         init_comp_def(
             ctx.accounts,
             Some(CircuitSource::OffChain(OffChainCircuitSource {
-                source: "https://raw.githubusercontent.com/Aren1414/Arciumint/main/arcium/disc_mpc/build/compute_disc.arcis"
-                    .to_string(),
-                hash: [0; 32],
+                source: "https://raw.githubusercontent.com/Aren1414/Arciumint/main/arcium/disc_mpc/build/compute_disc.arcis".to_string(),
+                hash: [0; 32], 
             })),
             None,
         )?;
@@ -29,70 +28,28 @@ pub mod disc_mpc {
     pub fn compute_disc(
         ctx: Context<ComputeDisc>,
         computation_offset: u64,
-        ciphertext_0: [u8; 32],
-        ciphertext_1: [u8; 32],
-        ciphertext_2: [u8; 32],
-        ciphertext_3: [u8; 32],
-        ciphertext_4: [u8; 32],
-        ciphertext_5: [u8; 32],
-        ciphertext_6: [u8; 32],
-        ciphertext_7: [u8; 32],
-        ciphertext_8: [u8; 32],
-        ciphertext_9: [u8; 32],
-        ciphertext_10: [u8; 32],
-        ciphertext_11: [u8; 32],
-        ciphertext_12: [u8; 32],
-        ciphertext_13: [u8; 32],
-        ciphertext_14: [u8; 32],
-        ciphertext_15: [u8; 32],
-        ciphertext_16: [u8; 32],
-        ciphertext_17: [u8; 32],
-        ciphertext_18: [u8; 32],
-        ciphertext_19: [u8; 32],
-        ciphertext_20: [u8; 32],
-        ciphertext_21: [u8; 32],
-        ciphertext_22: [u8; 32],
-        ciphertext_23: [u8; 32],
-        ciphertext_24: [u8; 32],
-        ciphertext_25: [u8; 32],
-        ciphertext_26: [u8; 32],
-        ciphertext_27: [u8; 32],
         pubkey: [u8; 32],
         nonce: u128,
+        c0: [u8; 32], c1: [u8; 32], c2: [u8; 32], c3: [u8; 32],
+        c4: [u8; 32], c5: [u8; 32], c6: [u8; 32], c7: [u8; 32],
+        c8: [u8; 32], c9: [u8; 32], c10: [u8; 32], c11: [u8; 32],
+        c12: [u8; 32], c13: [u8; 32], c14: [u8; 32], c15: [u8; 32],
+        c16: [u8; 32], c17: [u8; 32], c18: [u8; 32], c19: [u8; 32],
+        c20: [u8; 32], c21: [u8; 32], c22: [u8; 32], c23: [u8; 32],
+        c24: [u8; 32], c25: [u8; 32], c26: [u8; 32], c27: [u8; 32],
     ) -> Result<()> {
         ctx.accounts.sign_pda_account.bump = ctx.bumps.sign_pda_account;
 
         let args = ArgBuilder::new()
             .x25519_pubkey(pubkey)
             .plaintext_u128(nonce)
-            .encrypted_u8(ciphertext_0)
-            .encrypted_u8(ciphertext_1)
-            .encrypted_u8(ciphertext_2)
-            .encrypted_u8(ciphertext_3)
-            .encrypted_u8(ciphertext_4)
-            .encrypted_u8(ciphertext_5)
-            .encrypted_u8(ciphertext_6)
-            .encrypted_u8(ciphertext_7)
-            .encrypted_u8(ciphertext_8)
-            .encrypted_u8(ciphertext_9)
-            .encrypted_u8(ciphertext_10)
-            .encrypted_u8(ciphertext_11)
-            .encrypted_u8(ciphertext_12)
-            .encrypted_u8(ciphertext_13)
-            .encrypted_u8(ciphertext_14)
-            .encrypted_u8(ciphertext_15)
-            .encrypted_u8(ciphertext_16)
-            .encrypted_u8(ciphertext_17)
-            .encrypted_u8(ciphertext_18)
-            .encrypted_u8(ciphertext_19)
-            .encrypted_u8(ciphertext_20)
-            .encrypted_u8(ciphertext_21)
-            .encrypted_u8(ciphertext_22)
-            .encrypted_u8(ciphertext_23)
-            .encrypted_u8(ciphertext_24)
-            .encrypted_u8(ciphertext_25)
-            .encrypted_u8(ciphertext_26)
-            .encrypted_u8(ciphertext_27)
+            .encrypted_u8(c0).encrypted_u8(c1).encrypted_u8(c2).encrypted_u8(c3)
+            .encrypted_u8(c4).encrypted_u8(c5).encrypted_u8(c6).encrypted_u8(c7)
+            .encrypted_u8(c8).encrypted_u8(c9).encrypted_u8(c10).encrypted_u8(c11)
+            .encrypted_u8(c12).encrypted_u8(c13).encrypted_u8(c14).encrypted_u8(c15)
+            .encrypted_u8(c16).encrypted_u8(c17).encrypted_u8(c18).encrypted_u8(c19)
+            .encrypted_u8(c20).encrypted_u8(c21).encrypted_u8(c22).encrypted_u8(c23)
+            .encrypted_u8(c24).encrypted_u8(c25).encrypted_u8(c26).encrypted_u8(c27)
             .build();
 
         queue_computation(
@@ -123,18 +80,61 @@ pub mod disc_mpc {
 
         emit!(DiscScoresEvent {
             computation_account: ctx.accounts.computation_account.key(),
-            d_score_cipher: o.field_0.ciphertexts[0],
-            i_score_cipher: o.field_0.ciphertexts[1],
-            s_score_cipher: o.field_0.ciphertexts[2],
-            c_score_cipher: o.field_0.ciphertexts[3],
-            nonce: o.field_0.nonce.to_le_bytes(),
+            d_score_cipher: o.ciphertexts[0],
+            i_score_cipher: o.ciphertexts[1],
+            s_score_cipher: o.ciphertexts[2],
+            c_score_cipher: o.ciphertexts[3],
+            nonce: o.nonce.to_le_bytes(),
         });
 
         Ok(())
     }
 }
 
-// ---------- CALLBACK ACCOUNTS (NO CUSTOM ERRORS) ----------
+#[queue_computation_accounts("compute_disc", payer)]
+#[derive(Accounts)]
+#[instruction(computation_offset: u64)]
+pub struct ComputeDisc<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        space = 9,
+        payer = payer,
+        seeds = [&SIGN_PDA_SEED],
+        bump,
+        address = derive_sign_pda!(),
+    )]
+    pub sign_pda_account: Account<'info, SignerAccount>,
+
+    #[account(address = derive_mxe_pda!())]
+    pub mxe_account: Account<'info, MXEAccount>,
+
+    #[account(mut, address = derive_mempool_pda!(mxe_account, DiscMpcError::ClusterNotSet))]
+    pub mempool_account: UncheckedAccount<'info>,
+
+    #[account(mut, address = derive_execpool_pda!(mxe_account, DiscMpcError::ClusterNotSet))]
+    pub executing_pool: UncheckedAccount<'info>,
+
+    #[account(mut, address = derive_comp_pda!(computation_offset, mxe_account, DiscMpcError::ClusterNotSet))]
+    pub computation_account: UncheckedAccount<'info>,
+
+    #[account(address = derive_comp_def_pda!(COMP_DEF_OFFSET_COMPUTE_DISC))]
+    pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+
+    #[account(mut, address = derive_cluster_pda!(mxe_account, DiscMpcError::ClusterNotSet))]
+    pub cluster_account: Account<'info, Cluster>,
+
+    #[account(mut, address = ARCIUM_FEE_POOL_ACCOUNT_ADDRESS)]
+    pub pool_account: Account<'info, FeePool>,
+
+    #[account(mut, address = ARCIUM_CLOCK_ACCOUNT_ADDRESS)]
+    pub clock_account: Account<'info, ClockAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub arcium_program: Program<'info, Arcium>,
+}
 
 #[callback_accounts("compute_disc")]
 #[derive(Accounts)]
@@ -147,18 +147,42 @@ pub struct ComputeDiscCallback<'info> {
     #[account(address = derive_mxe_pda!())]
     pub mxe_account: Account<'info, MXEAccount>,
 
-    /// CHECK
     pub computation_account: UncheckedAccount<'info>,
 
-    #[account(address = derive_cluster_pda!(mxe_account))]
+    #[account(address = derive_cluster_pda!(mxe_account, DiscMpcError::ClusterNotSet))]
     pub cluster_account: Account<'info, Cluster>,
 
     #[account(address = ::anchor_lang::solana_program::sysvar::instructions::ID)]
     pub instructions_sysvar: AccountInfo<'info>,
 }
 
+#[init_computation_definition_accounts("compute_disc", payer)]
+#[derive(Accounts)]
+pub struct InitComputeDiscCompDef<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(mut, address = derive_mxe_pda!())]
+    pub mxe_account: Box<Account<'info, MXEAccount>>,
+    #[account(mut)]
+    pub comp_def_account: UncheckedAccount<'info>,
+    pub arcium_program: Program<'info, Arcium>,
+    pub system_program: Program<'info, System>,
+}
+
+#[event]
+pub struct DiscScoresEvent {
+    pub computation_account: Pubkey,
+    pub d_score_cipher: [u8; 32],
+    pub i_score_cipher: [u8; 32],
+    pub s_score_cipher: [u8; 32],
+    pub c_score_cipher: [u8; 32],
+    pub nonce: [u8; 16],
+}
+
 #[error_code]
 pub enum DiscMpcError {
     #[msg("The computation was aborted")]
     AbortedComputation,
-        }
+    #[msg("Cluster not set")]
+    ClusterNotSet,
+}
