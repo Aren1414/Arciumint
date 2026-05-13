cba4bff7ed93981777f51d581d01b07b6ecfed98
Aren <Aren.silver12@gmail.com>
2026-05-14 01:01:54 +0330

    Refactor DiscInput and DiscOutput structs

--- arcium/disc_mpc/encrypted-ixs/src/lib.rs
+++ arcium/disc_mpc/encrypted-ixs/src/lib.rs
@@ -1,44 +1,42 @@
-use arcis_imports::*;
+use arcis::*;
 
 #[encrypted]
 mod circuits {
-    use arcis_imports::*;
+    use arcis::*;
 
-        pub struct DiscInput {
-                pub answers: [u8; 28],
-                    }
+    pub struct DiscInput {
+        pub answers: [u8; 28],
+    }
 
-                        pub struct DiscOutput {
-                                pub d_score: u8,
-                                        pub i_score: u8,
-                                                pub s_score: u8,
-                                                        pub c_score: u8,
-                                                            }
+    pub struct DiscOutput {
+        pub d_score: u8,
+        pub i_score: u8,
+        pub s_score: u8,
+        pub c_score: u8,
+    }
 
-                                                                #[instruction]
-                                                                    pub fn compute_disc(
-                                                                            input_ctxt: Enc<Shared, DiscInput>
-                                                                                ) -> Enc<Shared, DiscOutput> {
-                                                                                        let input = input_ctxt.to_arcis();
+    #[instruction]
+    pub fn compute_disc(input_ctxt: Enc<Shared, DiscInput>) -> Enc<Shared, DiscOutput> {
+        let input = input_ctxt.to_arcis();
 
-                                                                                                let mut d = 0u8;
-                                                                                                        let mut i = 0u8;
-                                                                                                                let mut s = 0u8;
-                                                                                                                        let mut c = 0u8;
+        let mut d = 0u8;
+        let mut i = 0u8;
+        let mut s = 0u8;
+        let mut c = 0u8;
 
-                                                                                                                                for idx in 0..28 {
-                                                                                                                                            let v = input.answers[idx];
-                                                                                                                                                        d += (v == 0) as u8;
-                                                                                                                                                                    i += (v == 1) as u8;
-                                                                                                                                                                                s += (v == 2) as u8;
-                                                                                                                                                                                            c += (v == 3) as u8;
-                                                                                                                                                                                                    }
+        for idx in 0..28 {
+            let v = input.answers[idx];
+            d += (v == 0) as u8;
+            i += (v == 1) as u8;
+            s += (v == 2) as u8;
+            c += (v == 3) as u8;
+        }
 
-                                                                                                                                                                                                            input_ctxt.owner.from_arcis(DiscOutput {
-                                                                                                                                                                                                                        d_score: d,
-                                                                                                                                                                                                                                    i_score: i,
-                                                                                                                                                                                                                                                s_score: s,
-                                                                                                                                                                                                                                                            c_score: c,
-                                                                                                                                                                                                                                                                    })
-                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                        }
+        input_ctxt.owner.from_arcis(DiscOutput {
+            d_score: d,
+            i_score: i,
+            s_score: s,
+            c_score: c,
+        })
+    }
+}
