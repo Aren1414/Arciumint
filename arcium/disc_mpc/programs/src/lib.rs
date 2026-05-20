3082c309eccc396dacf7758cd384a72201293871
Aren <Aren.silver12@gmail.com>
2026-05-20 18:51:24 +0330

    Update lib.rs

--- arcium/disc_mpc/programs/src/lib.rs
+++ arcium/disc_mpc/programs/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 use arcium_anchor::prelude::*;
-use crate::__client_accounts_compute_disc_callback::ComputeDiscCallback as _; 
+use crate::__client_accounts_compute_disc_callback::ComputeDiscCallback;
 
 const COMP_DEF_OFFSET_COMPUTE_DISC: u32 = comp_def_offset("compute_disc");
 
@@ -195,7 +195,7 @@ pub struct InitComputeDiscCompDef<'info> {
     )]
     pub address_lookup_table: UncheckedAccount<'info>,
 
-    
+    /// CHECK: 
     #[account(address = LUT_PROGRAM_ID)]
     pub lut_program: UncheckedAccount<'info>,
 
